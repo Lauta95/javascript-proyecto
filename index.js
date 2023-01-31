11e0567eb83a05b7e21be9d25fb757d9f8b7748e
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-01-31 15:47:56 -0300

    Cambio de for por un while y optimizacion del codigo reduciendo lineas

--- index.js
+++ index.js
@@ -13,42 +13,36 @@ function Juego(nombre, precio) {
     this.precio = precio;
 }
 
-const juego1 = new Juego('resident evil', '$3500');
-const juego2 = new Juego('battlefield', '$2000');
-const juego3 = new Juego('gta', '$2500');
+const juego1 = new Juego('resident evil', 3500);
+const juego2 = new Juego('battlefield', 2000);
+const juego3 = new Juego('gta', 2500);
 
 
-for (let i = 0; i < 15; i++) {
+while (ingresar != 'salir') {
     if (ingresar == juego1.nombre) {
-        let agregar = prompt(`el precio del juego es de ${juego1.precio}, ¿desea agregar al carrito?`);
+        let agregar = prompt(`el precio del juego es de $${juego1.precio}, ¿desea agregar al carrito?`);
         if (agregar == 'agregar' || agregar == 'si') {
             alert('agregado');
-            ingresar = prompt('Ingresar nombre del juego');
         } else {
             alert('juego no agregado');
-            ingresar = prompt('Ingresar nombre del juego');
         }
     } else if (ingresar == juego2.nombre) {
-        let agregar = prompt(`el precio del juego es de ${juego2.precio}, ¿desea agregar al carrito?`);
+        let agregar = prompt(`el precio del juego es de $${juego2.precio}, ¿desea agregar al carrito?`);
         if (agregar == 'agregar' || agregar == 'si') {
             alert('agregado');
-            ingresar = prompt('Ingresar nombre del juego');
         } else {
             alert('juego no agregado');
-            ingresar = prompt('Ingresar nombre del juego');
         }
     } else if (ingresar == juego3.nombre) {
-        let agregar = prompt(`el precio del juego es de ${juego3.precio}, ¿desea agregar al carrito?`);
+        let agregar = prompt(`el precio del juego es de $${juego3.precio}, ¿desea agregar al carrito?`);
         if (agregar == 'agregar' || agregar == 'si') {
             alert('agregado');
-            ingresar = prompt('Ingresar nombre del juego');
         } else {
             alert('juego no agregado');
-            ingresar = prompt('Ingresar nombre del juego');
         }
     } else {
         alert(`${ingresar} no está disponible`);
-        ingresar = prompt('Ingresar nombre del juego');
     }
+    ingresar = prompt('Ingresar nombre del juego');
 }
 
