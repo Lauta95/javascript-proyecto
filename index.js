10fe24fcbfa51e494c4747e0e2cbc61165fe41f1
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-02-04 07:22:58 -0300

    Se agrega la posibilidad de poner un nombre de usuario y a medida que vas agregando un juego el precio total se va sumando.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
-alert('Bienvenido. Primero ingrese el nombre del juego que desea comprar');
+let nombreUsuario = prompt('Ingrese su nombre');
 
-alert(`Lista de juegos disponibles:
+alert(`Bienvenido ${nombreUsuario}. Primero ingrese el nombre del juego que desea comprar`);
+
+alert(`${nombreUsuario}, esta es nuestra lista de juegos disponibles:
 -resident evil
 -battlefield
 -gta`);
@@ -17,11 +19,13 @@ const juego1 = new Juego('resident evil', 3500);
 const juego2 = new Juego('battlefield', 2000);
 const juego3 = new Juego('gta', 2500);
 
+let totalCompra = 0;
 
 while (ingresar != 'salir') {
     if (ingresar == juego1.nombre) {
         let agregar = prompt(`el precio del juego es de $${juego1.precio}, ¿desea agregar al carrito?`);
         if (agregar == 'agregar' || agregar == 'si') {
+            totalCompra += juego1.precio;
             alert('agregado');
         } else {
             alert('juego no agregado');
@@ -29,6 +33,7 @@ while (ingresar != 'salir') {
     } else if (ingresar == juego2.nombre) {
         let agregar = prompt(`el precio del juego es de $${juego2.precio}, ¿desea agregar al carrito?`);
         if (agregar == 'agregar' || agregar == 'si') {
+            totalCompra += juego2.precio;
             alert('agregado');
         } else {
             alert('juego no agregado');
@@ -36,6 +41,7 @@ while (ingresar != 'salir') {
     } else if (ingresar == juego3.nombre) {
         let agregar = prompt(`el precio del juego es de $${juego3.precio}, ¿desea agregar al carrito?`);
         if (agregar == 'agregar' || agregar == 'si') {
+            totalCompra += juego3.precio;
             alert('agregado');
         } else {
             alert('juego no agregado');
@@ -43,6 +49,6 @@ while (ingresar != 'salir') {
     } else {
         alert(`${ingresar} no está disponible`);
     }
+    alert(`Precio total $${totalCompra}`);
     ingresar = prompt('Ingresar nombre del juego');
 }
-
