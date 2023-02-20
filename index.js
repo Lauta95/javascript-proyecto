dd9fe3772c81a8592087f3a9a8e761850541be30
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-02-20 12:21:01 -0300

    Se agrega html y bootstrap

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 // Array de objetos
 const juegos = [
-    { id: 1, nombre: 'dark souls', precio: 1500 },
-    { id: 2, nombre: 'gta', precio: 2500 },
+    { id: 1, nombre: 'dark souls', precio: 8599 },
+    { id: 2, nombre: 'gta', precio: 5698 },
     { id: 3, nombre: 'resident evil', precio: 5500 },
-    { id: 4, nombre: 'counter strike', precio: 100 },
+    { id: 4, nombre: 'god of war', precio: 4199 },
     { id: 5, nombre: 'age of empires', precio: 500 },
     { id: 6, nombre: 'hitman', precio: 1450 },
     { id: 7, nombre: 'outlast', precio: 1000 },
     { id: 8, nombre: 'god of war', precio: 2575 }
 ];
-// Ingresar nombre de usuario
-let usuario = prompt(`Hola, ingrese su nombre por favor`);
+
 // Método para mostrar los juegos
 const nombres = juegos.map(juego => juego.nombre);
-
-alert(`¡Bienvenido ${usuario}! Este es nuestro listado de juegos:
-
-${nombres.join('\n')}`);
 
 let totalCompra = 0;
 
