83c9b980e41cac285dce43cb17866c8ff12bee3d
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-03-26 21:46:49 -0300

    Se agrega la suma de los juegos

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ class Carrito {
     quitarJuego(id) {
         this.carrito = this.carrito.filter(juego => juego.id !== id);
     }
+
+    getCarrito() {
+      return this.carrito;
+    }
+
     // get
     listarJuegos() {
         return this.carrito.map(juego => `<li><a class="dropdown-item" href="#">${juego.nombre}: $${juego.precio}</a></li>`);
@@ -33,6 +38,15 @@ class Carrito {
         if (guardar) {
             this.carrito = JSON.parse(guardar);
         }
+    }
+    mostrarTotal(){
+        let total = 0;
+        for (let i = 0; i < this.carrito.length; i++) {
+            const juego = this.carrito[i] // -> Juego;
+            total += juego.precio;
+        }
+        return `
+        El total es $${total}`;
     }
 }
 // Se crea un nuevo carro para guardar los elementos en la clase carrito
@@ -51,6 +65,17 @@ let juego8 = new Juego(8, "Spider Man", 8748);
 const juegos = [juego1, juego2, juego3, juego4, juego5, juego6, juego7, juego8];
 
 function botonComprar(id) {
+    // ver si ya existe con un condicional if si da verdadero es porque encontró un juego que ya esta en el carrito
+    if(nuevoCarro.getCarrito().find(juego => juego.id == id)) {
+        Swal.fire({
+            position: 'center',
+            icon: 'warning',
+            title: 'No puedes comprar el mismo juego',
+            showConfirmButton: false,
+            timer: 1500
+          })
+      return
+    }
     for (let i = 0; i < juegos.length; i++) {
         if (id == juegos[i].id) {
             nuevoCarro.agregarJuego(juegos[i]);
@@ -76,4 +101,5 @@ function botonQuitar(id) {
 
 const listarEnCarrito = document.getElementById("listarEnCarrito");
 
-listarEnCarrito.onclick = () => document.getElementById("lista").innerHTML = nuevoCarro.listarJuegos();
+listarEnCarrito.onclick = () => document.getElementById("lista").innerHTML = nuevoCarro.listarJuegos() + nuevoCarro.mostrarTotal();
+
