cf6a4293a1d3dcd12300e679b34b0f8120758bf0
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-04-10 21:24:42 -0300

    se agrega una funcion asincrona para traer los juegos desde un archivo json

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ class Carrito {
         let total = 0;
         for (let i = 0; i < this.carrito.length; i++) {
             const juego = this.carrito[i]
-            total += juego.precio;
+            total += parseInt(juego.precio);
         }
         return `
         <hr class="dropdown-divider">
@@ -48,16 +48,21 @@ class Carrito {
 // Se crea un nuevo carro para guardar los elementos en la clase carrito
 let nuevoCarro = new Carrito();
 nuevoCarro.cargarLocal()
-// Se cargan los juegos con un new en las clases
-let juego1 = new Juego(1, "Dark Souls 3", 8599);
-let juego2 = new Juego(2, "Gta V", 5698);
-let juego3 = new Juego(3, "Resident Evil 4", 5500);
-let juego4 = new Juego(4, "God Of War", 4199);
-let juego5 = new Juego(5, "Dead Space", 13998);
-let juego6 = new Juego(6, "Rust", 4271);
-let juego7 = new Juego(7, "Red Dead Redemption", 19773);
-let juego8 = new Juego(8, "Spider Man", 8748);
-const juegos = [juego1, juego2, juego3, juego4, juego5, juego6, juego7, juego8];
+
+
+// Se arma una función asíncrona para traer los juegos desde el archivo json
+let juegos;
+async function traerJuegos() {
+    const response = await fetch("./juegos.json")
+
+    const result = await response.json();
+
+    juegos = result.juegos.map(juego => new Juego(juego.id, juego.nombre, juego.precio));
+    console.log(juegos);
+}
+
+traerJuegos();
+
 function botonComprar(id) {
     // El primer condicional es para ver si ya existe, si da verdadero es porque encontró un juego que ya esta en el carrito.
     if (nuevoCarro.getCarrito().find(juego => juego.id == id)) {
@@ -85,6 +90,7 @@ function botonComprar(id) {
     }
 }
 function botonQuitar(id) {
+    console.log(juegos);
     for (let i = 0; i < juegos.length; i++) {
         if (id == juegos[i].id) {
             nuevoCarro.quitarJuego(id);
